5c635a7bc668f7de27792dd8af7d19e401c094fc
Prem Prakash Rohan <prem.prakash.rohan@gmail.com>
2022-06-16 09:03:14 +0530

    create

---
+++ src/main/java/maven/test/VersioningDemo.java
@@ -0,0 +1,12 @@
+package maven.test;
+
+public class VersioningDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println(VersioningDemo.class.getPackage().getImplementationVersion());
+
+	}
+
+}
